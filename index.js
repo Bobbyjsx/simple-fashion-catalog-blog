5caee610179e725331ce0668aa0eec801f21b718
Bobby <104563882+akarasauce@users.noreply.github.com>
2022-11-07 12:07:49 -0800

    added new functions to the subscribe button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function display(){
     document.getElementById("btn-el").innerHTML= "We have recieved your input, we'll contact you soonest.😌" 
- 
+     document.getElementById("input-el").value=""
     return
 }
